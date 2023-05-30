5c05d1682c21b243b30dac10e65b6a4b912f5e6d
Deepa1172 <74766365+Deepa1172@users.noreply.github.com>
2023-05-30 20:05:25 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
-#flask = library, Flask = Class 
+from flask import Flask, render_template, jsonify
+#flask = library, Flask = Class
 app = Flask(__name__)
+CATS = [{
+  "name": "Cash",
+  "rank": 1,
+  "breed": "British Shorthair",
+  "Instagram": "@meow_cash"
+}, {
+  "name": "Kitty",
+  "rank": 2,
+  "breed": "Bengal Cat",
+  "Instagram": "@bengalkittycat"
+}, {
+  "name": "Meatball",
+  "rank": 3,
+  "breed": "Tuxedo",
+  "Instagram": "@meevin_official"
+}]
 
-@app.route("/") #part of the URL (path)
+
+@app.route("/")  #part of the URL (path)
 def hello_world():
-    return "Hello, World!"
+  return render_template('home.html',
+                         cats=CATS)  #figure out how to add icon in flask
+@app.route("/api/cats")
+def jsonif():
+  return jsonify(CATS)
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0', debug=True) #run locally
+  app.run(host='0.0.0.0', debug=True)  #run locally
